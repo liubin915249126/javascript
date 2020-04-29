4521ea191a56f53990ccbfce8be559bf2d7cdfd7
liubin <915249126@qq.com>
2020-04-29 13:52:21 +0800

    add

--- bybit/banner/data.js
+++ bybit/banner/data.js
@@ -10,3 +10,15 @@
 
     }
 ]
+
+[
+  {
+    title:'', //埋点
+    link:'',  //外链
+    banner:{
+      "img768": "/images/banner/768/usdt-airdrop.jpg",
+      "img1280": "/images/banner/1280/usdt-airdrop.jpg",
+      "img1920": "/images/banner/1920/usdt-airdrop.jpg"
+    },
+  }
+]
