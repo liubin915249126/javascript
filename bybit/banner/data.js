3a2d0fd4225ac528650fd380b630f48c595921ae
liubin <915249126@qq.com>
2020-04-29 16:04:31 +0800

    modify jp-news banner

--- bybit/banner/data.js
+++ bybit/banner/data.js
@@ -13,12 +13,11 @@
 
 [
   {
-    title:'', //埋点
+    name:'', //埋点
     link:'',  //外链
-    banner:{
-      "img768": "/images/banner/768/usdt-airdrop.jpg",
-      "img1280": "/images/banner/1280/usdt-airdrop.jpg",
-      "img1920": "/images/banner/1920/usdt-airdrop.jpg"
-    },
+    title:'', //卡片说明banner没有
+    img768: "/images/banner/768/usdt-airdrop.jpg",
+    img1280: "/images/banner/1280/usdt-airdrop.jpg",
+    img1920: "/images/banner/1920/usdt-airdrop.jpg"
   }
 ]
