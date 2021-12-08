3ac813aba5de704311a871482cad1f2937569314
len.liu <len.liu@yijinin.com>
2021-12-08 18:35:28 +0800

    feat(javascript): add util

--- utils/utils.js
+++ utils/utils.js
@@ -4,3 +4,12 @@ export const objToArr = (obj)=>{
       name:obj[key],
     }))
   }
+
+export const  downLoadFile = (res)=>{
+  const blob = res.blob()
+  var url = window.URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  a.href = url;
+  a.download = "filename.xlsx";
+  a.click();
+}
